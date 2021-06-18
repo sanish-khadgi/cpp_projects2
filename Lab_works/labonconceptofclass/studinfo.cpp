05b5273fc2170368f3dd48ba56bce80968db10fc
sanish-khadgi <123sanishkhadgi@gmail.com>
2021-06-18 09:12:03 +0545

    student's information using 'Class'methode

---
+++ Lab_works/labonconceptofclass/studinfo.cpp
@@ -0,0 +1,52 @@
+//WAP to define a class in C++ as shown : class name- Student attributes - name, roll no., address, percentage; method-input(), display();
+#include<iostream>
+#include<conio.h>
+using namespace std;
+ 
+//creating a class;
+class student {
+
+     string name;
+     int roll_no;
+     float percentage;  
+     string address;
+
+    public:
+
+     void input(){
+      cout<<"Enter Name: ";
+      cin>>name;
+
+      cout<<"Enter Roll no.: ";
+      cin>>roll_no;
+
+      cout<<"Enter Address";
+      cin>>address;
+
+      cout<<"Enter Percentge";
+      cin>>percentage;
+     
+     }
+     void display(){
+         if(percentage<40){
+             cout<<"Name:\t"<<name<<"\nRoll no:\t"<<roll_no<<"\nAddress:\t"<<address<<"\nPercentage:\t"<<percentage<<":Failed";  
+         }
+         else{
+             cout<<"Name:\t"<<name<<"\nRoll no:\t"<<roll_no<<"\nAddress:\t"<<address<<"\nPercentage:\t"<<percentage<<":Passed";   
+         }
+     }
+
+};
+
+int main(){
+    //creating object
+    student s1,s2;
+    s1.input();
+    s2.input();
+
+    s1.display();
+    s2.display();
+ getch();
+
+ return 0;   
+}

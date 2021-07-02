4e205fb5dcedd6e5f3542e02ef14300536ed026a
sanish-khadgi <123sanishkhadgi@gmail.com>
2021-07-02 09:54:17 +0545

    test added

---
+++ Assignments/area_of_triangle-declration_of_function/without_function_declaration.cpp
@@ -0,0 +1,41 @@
+// area of triangle without funtion declaration 
+#include<iostream>
+#include<math.h>
+#include<conio.h>
+
+using namespace std;
+
+float area_Triangle(float a, float b, float c){//if main funtion is written at last; the function declaration is not needed as there is already the function at top as flow of program  
+float s, area;
+
+s=(a+b+c)/2;
+
+area=sqrt(s*(s-a)*(s-b)*(s-c));
+
+
+return area;
+}
+
+int main(){
+
+    float area, a, b, c, s;
+
+    cout<<"Enter the sides of the triangle";
+
+    cout<<"Side a : ";
+    cin>>a;
+
+    cout<<"Side b : ";
+    cin>>b;
+
+    cout<<"Side c : ";
+    cin>>c;
+    
+    area=area_Triangle(a, b, c); //funtion call
+
+    cout<<"The area of the triangle is : "<<area;
+
+     getch();
+
+     return 0;
+}
